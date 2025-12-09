9564aae5e6d39db27436746e1fdc3b217ba6bf35
Shwetank Tripathi <swetankvns@gmail.com>
2025-12-09 22:55:05 +0530

    minor cors fixes

--- server/index.js
+++ server/index.js
@@ -18,11 +18,11 @@ const PORT = process.env.PORT || 3000;
 //For HTTP requests
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL || "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "roomid", "socketid"],
-  }),
+  })
 );
 
 app.use(cookieParser());
