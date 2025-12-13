21ca46a5cb833616ae7ea47701fd52761440bfa7
Shwetank Tripathi <swetankvns@gmail.com>
2025-12-13 12:52:52 +0530

    minor cors fix

--- server/index.js
+++ server/index.js
@@ -15,15 +15,12 @@ if (process.env.NODE_ENV !== "production") {
 
 const PORT = process.env.PORT || 3000;
 
-//For HTTP requests
-app.use(
-  cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL || "http://localhost:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "roomid", "socketid"],
-  })
-);
+app.use(cors({
+  origin:["http://localhost:5173", process.env.FRONTEND_URL],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "roomid", "socketid"],
+}));
 
 app.use(cookieParser());
 app.use(express.json()); //built-in function in Express.js used to parse incoming requests with JSON payloads
@@ -37,9 +34,10 @@ app.use(auth);
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 200,
   standardHeaders: true,
   legacyHeaders: false,
+  message: "Too many requests from this IP, please try again after 15 minutes.",
 });
 app.use("/user", apiLimiter, userRoutes);
 app.use("/task", apiLimiter, taskRoutes);
